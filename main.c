b730cda0256df432667afcc53fd2f862a3212a09
bothainakb <bothainakb@gmail.com>
2026-05-13 09:18:21 +0000

    main.c

--- main.c
+++ main.c
@@ -1,51 +1,53 @@
 #include "shell.h"
 
 /**
- * main - Entry point for the simple shell
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 on success
+ * main - simple shell
+ * Return: 0
  */
-int main(int argc, char **argv)
+int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t nread;
-	char *args[64];
+	ssize_t read;
+	char *token;
+	char *argv[64];
 	int i;
-	(void)argc;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
+			printf("($) ");
 
-		nread = getline(&line, &len, stdin);
+		read = getline(&line, &len, stdin);
 
-		if (nread == -1)
+		if (read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
 			free(line);
-			exit(EXIT_SUCCESS);
+			exit(0);
 		}
+
+		if (line[0] == '\n')
+			continue;
+
+		line[strcspn(line, "\n")] = '\0';
+
+		token = strtok(line, " ");
 
 		i = 0;
-		args[i] = strtok(line, " \n\t\r");
-		while (args[i] != NULL && i < 63)
-		{
-			i++;
-			args[i] = strtok(NULL, " \n\t\r");
-		}
-		args[i] = NULL;
 
-		if (args[0] != NULL)
+		while (token != NULL)
 		{
-			execute_cmd(args, argv[0], line);
+			argv[i++] = token;
+			token = strtok(NULL, " ");
 		}
+
+		argv[i] = NULL;
+
+		if (argv[0] != NULL)
+			execute_command(argv);
 	}
 
 	free(line);
+
 	return (0);
 }
